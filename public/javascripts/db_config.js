d52b4d7c574eff8e23cc3107526434a020ea84c7
Vue.js2 <hiroki.k.0323@docomo.ne.jp>
2022-12-19 15:30:00 +0900

    1/19

---
+++ public/javascripts/db_config.js
@@ -0,0 +1,11 @@
+xports.mysql_setting = {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'graduationwork',
+    timezone: 'jst'
+};
+exports.loginSQL = 'SELECT * from customer where user_id=? AND password=?'
+exports.insertRecordSQL = 'INSERT INTO chat_histories(history_index,user_id,message,date_time) VALUES(?, ?, ?, ?)';
+exports.getHistoriesSQL = 'SELECT * FROM chat_histories';
+exports.deleteHistorySQL = 'TRUNCATE TABLE chat_histories';
